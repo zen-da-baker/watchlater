a0062bd3c2c8c1d3804d5e7764c033b01b4ebd08
Don <120154112+zen-da-baker@users.noreply.github.com>
2025-05-10 23:29:50 -0700

    Media class created

--- watchlater/src/classes/Media.ts
+++ watchlater/src/classes/Media.ts
@@ -1,5 +1,15 @@
 export class Media {
 
-    
+    title: string = "";
+
+    tags: Array<string> = [];
+
+    constructor(title: string = "", tags: Array<string> = []) {
+
+        this.title = title;
+
+        this.tags = tags;
+        
+    }
 
 }
